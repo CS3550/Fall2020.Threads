573ef53c68e8a1813fb2a191ba37b77ec0a73ff7
B. Ricks <bricks@unomaha.edu>
2020-08-27 15:02:04 -0500

    Add Main

---
+++ src/main/java/cs3550/fall2020/threads/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs3550.fall2020.threads;
+
+/**
+ *
+ * @author bricks
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        System.out.println("Hi");
+    }
+    
+}
