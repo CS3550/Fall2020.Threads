7e828e7ff9c2ae59f037310ff28150318061f7e7
B. Ricks <bricks@unomaha.edu>
2020-08-27 15:26:53 -0500

    Add Java 7 version for clarity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,9 +10,11 @@ public class Main {
             
             serialDeposit();
             
-            unsafeThreadedDeposit();
+            //unsafeThreadedDepositOld();
+            
+            //unsafeThreadedDeposit();
                         
-            safeThreadedDeposit();
+            //safeThreadedDeposit();
             
         } catch (Exception ex) {
             ex.printStackTrace();;
@@ -27,7 +29,54 @@ public class Main {
 		System.out.println("Two sequential for loops:");            
 		b.PrintBalance();
 	}
+	
+	
+	//Pre-2014 way
+	private static void unsafeThreadedDepositOld() throws InterruptedException {
+		//Thread code with unlocked code underneath.
+		
+		final ABankAccount b2 = new UnsafeAccount();
+		
+		//Create two runnable instances pointing to b2
+		Runnable unsafeRunnable1 = new Runnable() {
+			@Override
+			public void run() {
+				b2.DepositCash();
+				
+			}
+		};
+		
+		Runnable unsafeRunnable2 = new Runnable() {
+			@Override
+			public void run() {
+				b2.DepositCash();
+				
+			}
+		};
+		
+		
+		//Create two threads pointing to these runnables
+		
+		Thread unsafeThread1 = new Thread(unsafeRunnable1);
+		Thread unsafeThread2 = new Thread(unsafeRunnable2);
+		
+		//Start these threads
+		
+		unsafeThread1.start();
+		unsafeThread2.start();
+		
+		//Wait for these threads to finish
+		
+		unsafeThread1.join();
+		unsafeThread2.join();
+		
+		//Print the sum
+		
+		System.out.println("Two threads unlocked (old way)");
+		b2.PrintBalance();
+	}
 
+	//Java 8+
 	private static void unsafeThreadedDeposit() throws InterruptedException {
 		//Thread code with unlocked code underneath.
 		
